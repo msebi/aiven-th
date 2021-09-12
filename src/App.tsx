044551a8e0f87a53925f00069806820b9cb3b90c
Sebi <mdnssebastian@gmail.com>
2021-09-12 21:08:12 +0200

    Added geo location hook; adding admin panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { GetGeoLocationAPI } from './hooks/GetGeoLocationAPI';
 
 function App() {
+  const geoLocation = GetGeoLocationAPI()
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        aiven Cloud Selection Panel
       </header>
+      <body className="App-body">
+        <p>
+          {geoLocation.isGeoLocationDataSet
+            ? JSON.stringify(geoLocation)
+            : "Location data not available yet."}
+        </p>
+      </body>
     </div>
   );
 }
---
+++ src/hooks/GetGeoLocationAPI.tsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react'
+
+interface GeoLocationData {
+    isGeoLocationDataSet: boolean
+    lat: string
+    lon: string
+    errorCode: number,
+    errorMsg: string
+}
+
+export const GetGeoLocationAPI = (): GeoLocationData => {
+    const [geoLocationData, setGeolocationData] = useState<GeoLocationData>({
+        isGeoLocationDataSet: false,
+        lat: '',
+        lon: '',
+        errorCode: 0,
+        errorMsg: ''
+    });
+
+    const onSuccess = (location: { coords: { latitude: any; longitude: any; }; }) => {
+        setGeolocationData({
+            isGeoLocationDataSet: true,
+            lat: location.coords.latitude,
+            lon: location.coords.longitude,
+            errorCode: 0,
+            errorMsg: ''
+        })
+    }
+
+    const onError = (error: { isGeoLocationDataSet?: boolean; lat?: string; lon?: string; code?: number; message?: string; coords?: any; }) => {
+        setGeolocationData({
+            isGeoLocationDataSet: true,
+            lat: error.coords.latitude,
+            lon: error.coords.longitude,
+            errorCode: 0,
+            errorMsg: ''
+        })
+    }
+
+    useEffect(() => {
+        // https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API
+        if (!('geolocation' in navigator)) {
+            onError({
+                isGeoLocationDataSet: true,
+                coords: {
+                    lat: '',
+                    lon: '',
+                },
+                code: 1,
+                message: 'Geolocation not enabled. Cloud service providers might not be close to your location'
+            })
+        }
+        navigator.geolocation.getCurrentPosition(onSuccess, onError)
+
+    }, [])
+
+    return geoLocationData
+}
+
