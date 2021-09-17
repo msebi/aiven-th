39d9d95e5093728f218245d39a1b1a2d31600d9b
Sebi <mdnssebastian@gmail.com>
2021-09-17 22:16:07 +0200

    Added trivial caching mechanism

--- backend/backend/get_providers/models.py
+++ backend/backend/get_providers/models.py
@@ -1,22 +1,28 @@
 from django.db import models
+import datetime
+from django.utils import timezone
+
+
+class ProvidersCacheDate(models.Model):
+    cache_date = models.DateTimeField('Date providers were last fetched on')
+
+    def is_older_than(self, n_days=1):
+        return self.cache_date >= timezone.now() - datetime.timedelta(days=n_days)
+
+    def __str__(self):
+        return self.cache_date
 
 
 class Providers(models.Model):
     cloud_description = models.CharField(max_length=1000)
     cloud_name = models.CharField(max_length=1000)
-    cloud_distance = models.IntegerField(default=0)
     geo_latitude = models.FloatField(default=0.0)
     geo_longitude = models.FloatField(default=0.0)
-    geo_longitude = models.CharField(max_length=100)
+    geo_region = models.CharField(max_length=100)
+    fetched_date = models.ForeignKey(ProvidersCacheDate, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.cloud_name
 
 
-class ProvidersCacheDate(models.Model):
-    # 1 to 1 rel with providers
-    providers = models.ForeignKey(Providers, unique=True, on_delete=models.CASCADE)
-    cache_date = models.DateTimeField('Last date for getting providers')
 
-    def __str__(self):
-        return self.cache_date
--- backend/backend/get_providers/views.py
+++ backend/backend/get_providers/views.py
@@ -2,21 +2,76 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 import requests
+from django.utils import timezone
+import json
+
+from get_providers.models import ProvidersCacheDate, Providers
 
 
 def index(request):
     return HttpResponse("Backend API to fetch cloud providers")
 
 
-def get_providers(request):
-    url = 'https://api.aiven.io/v1/clouds'
-    response = requests.get(
+# TODO: add try/catch
+def make_rest_request(url='https://api.aiven.io/v1/clouds'):
+    return requests.get(
         url, headers={'Content-Type': 'application/json'}
     )
 
+
+# TODO: add parameter validation
+def add_provider_entry(response):
+    response_json = response.json()
+    cache_date = ProvidersCacheDate.objects.create(cache_date=timezone.now())
+    fetched_providers = response_json.get('clouds')
+    for provider in fetched_providers:
+        provider_entry = Providers(cloud_description=provider.get('cloud_description'),
+                                   cloud_name=provider.get('cloud_name'),
+                                   geo_latitude=provider.get('geo_latitude'),
+                                   geo_longitude=provider.get('geo_longitude'),
+                                   geo_region=provider.get('geo_region'),
+                                   fetched_date=cache_date)
+        provider_entry.save()
+
+
+def return_json_response(response):
     return HttpResponse(
         content=response.content,
         status=response.status_code,
         content_type=response.headers['Content-Type']
     )
+
+
+def get_providers_rest():
+    response = make_rest_request()
+    add_provider_entry(response)
+    return return_json_response(response)
+
+
+def get_providers(request):
+    # cache response for a day
+    # https://django.readthedocs.io/en/stable/topics/db/models.html#extra-fields-on-many-to-many-relationships
+    # TODO: add use cases for empty providers list, service unavailable (server side errors), no Internet connection
+    if ProvidersCacheDate.objects.count() == 0:
+        return get_providers_rest()
+
+    if ProvidersCacheDate.objects.filter(id=1).is_older_than():
+        # clear current cache entry
+        Providers.objects.all().delete()
+        get_providers_rest()
+    else:
+        # return cached providers
+        # TODO: abstract hardcoded strings; create helper function
+        response_json = {'clouds': []}
+        for provider in Providers.objects.all():
+            provider_json = {
+                'cloud_description': provider['cloud_description'],
+                'cloud_name': provider['cloud_name'],
+                'geo_latitude': provider['geo_latitude'],
+                'geo_longitude': provider['geo_longitude'],
+                'geo_region': provider['geo_region'],
+            }
+            response_json['clouds'].append(provider_json)
+
+        return JsonResponse(response_json)
 
