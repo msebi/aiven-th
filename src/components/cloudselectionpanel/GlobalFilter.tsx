89ddee450cf4e63f550df4cfd76ac86ba12d60be
Sebi <mdnssebastian@gmail.com>
2021-09-19 20:11:07 +0200

    Added debounce to search function

--- src/components/cloudselectionpanel/GlobalFilter.tsx
+++ src/components/cloudselectionpanel/GlobalFilter.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAsyncDebounce } from 'react-table'
 
 type GlobalFilterProps = {
     filter: string, 
@@ -6,11 +7,22 @@ type GlobalFilterProps = {
 }
 
 export const GlobalFilter = ({filter, setFilter}: GlobalFilterProps) => {
+    const [value, setValue] = useState(filter)
+    // https://react-table.tanstack.com/docs/faq#how-can-i-debounce-rapid-table-state-changes
+    const onChange = useAsyncDebounce(value => {
+        setFilter(value || undefined)
+    }, 300)
+
     return (
         <span>
             Search results: {' '}
-            <input value={filter || ''} 
-            onChange = {(event: { target: { value: any } }) => setFilter(event.target.value)} />
+            <input value={value || ''} 
+            onChange = {(event: { target: { value: any } }) => 
+                {
+                    setValue(event.target.value)
+                    onChange(event.target.value)
+                }
+            } />
         </span>
     )
 }
